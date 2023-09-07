f6d3550ed04d7db93e6f0b13a47453b52cea7cab
Moham Roohan <76082817+mohamroohan@users.noreply.github.com>
2023-09-07 23:25:37 +0500

    C Print

---
+++ print.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(void){
+
+
+char s[] = "moiz moham";
+
+printf("----------MY FIRST PROGRAM----------");
+printf("\n");
+printf("--------------------------------------");
+printf("\n");.
+printf("My first string = %s",s);
+printf("\n");
+printf( "-------------------------------------------");
+
+
+
+}
