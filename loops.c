41c950937cb5d383333d11a4521913fe4c7e0187
Moham Roohan <76082817+mohamroohan@users.noreply.github.com>
2023-09-23 09:12:17 +0500

    loops

---
+++ loops.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+void main() {
+/////////while
+int i=1;
+while (i<11){
+printf("%d\n",i);
+i=i+1;
+}
+int j=131;
+while (j<=146){
+printf("%d\n",j);
+j=j+5;
+}
+
+/////////For
+
+for(int a = 1; a<=5; a=a+1){
+printf ("%d\n",a);
+
+
+
+}
+int k=1;
+for(;i<=5;){
+printf("%d\n",k);
+k=k+1;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int l=10;
+for(;l>=1;){
+printf("%d\n",l);
+l=l-1;
+}
+
+
+
+int d=1;
+printf ("Enter Table Number = ");
+scanf ("%d",&d);
+for(int c = 1;c<=10;c=c+1){
+
+printf("%d x %d = %d\n",d,c,c*d);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
